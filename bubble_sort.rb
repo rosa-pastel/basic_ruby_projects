e2b7072dc959a46b4f9562516e31c0013231e47b
rosa-pastel <zeynepranamutlu@outlook.com>
2021-10-03 15:43:29 +0300

    Completed bubble sort project for TOP

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,8 @@
 def bubble_sort(array)
-    array.size.times do
+    counter=0
+    (array.size-1).times do
         array.each_index do |index|
-            if index>0
+            if index>0 && index<=array.size-counter
                 if array[index]<array[index-1]
                     number=array[index-1]
                     array[index-1]=array[index]
@@ -9,7 +10,8 @@ def bubble_sort(array)
                 end
             end
         end
+        counter+=1
+        p array
     end
-    p array
 end
 bubble_sort([4,3,78,2,0,2])
